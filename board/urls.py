6fdec04c451cd05f15a264355fc1d4fa6e6e75fe
realmate01 <realmate01@realmate01ui-iMac.local>
2024-08-19 10:43:38 +0900

    1차 게시판 CRUD 완료

--- board/urls.py
+++ board/urls.py
@@ -6,4 +6,9 @@ app_name = 'board'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list/', views.lsit, name='list'),
+    path('read/<int:id>/', views.read, name='read'),
+    path('regist/', views.regist, name='regist'),
+    path('edcit/<int:id>/', views.edit, name='edit'),
+    path('remove/<int:id>/', views.remove, name='remove'),
 ]
